f4fb801e38cd0f42bc9adfe7c82497af143c27aa
PRABIN1804 <prabinmishra2004@gmail.com>
2023-01-07 09:37:37 +0530

    files added

---
+++ main.js
@@ -0,0 +1,14 @@
+// <!-- JAVASCRIPT PART
+
+    
+    const mobileBtn= document.getElementById("mobile-cta");
+    const nav=document.querySelector("nav");
+    const mobileBtnExit = document.getElementById("mobile-exit");
+
+    mobileBtn.addEventListener("click", () => {
+        nav.classList.add("menu-btn");
+    });
+
+    mobileBtnExit.addEventListener("click", () => {
+        nav.classList.remove("menu-btn");
+    });
